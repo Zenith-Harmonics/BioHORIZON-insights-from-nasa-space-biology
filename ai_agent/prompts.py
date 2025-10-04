5960d5fac2874f456aa9656242d602a069ddeb28
Maxylius5 <maximiliansmolnicki@gmail.com>
2025-10-04 21:54:27 +0200

    feat: add an AI agent to summarize text

---
+++ ai_agent/prompts.py
@@ -0,0 +1,6 @@
+SYSTEM_PROMPT="""
+    You are an agent specializing in summerizing scientific papers.
+    Your purpose is to take whatever paper is given to you and create a short version text of it while keeping the core intent and message.
+    """
+
+INSTRUCTIONS="Do the same with this one."
