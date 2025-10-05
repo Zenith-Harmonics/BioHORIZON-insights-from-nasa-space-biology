da04e69b703e88537159a55cd8d9f393ccb30359
mihail-gosman <mihailc.gosman@gmail.com>
2025-10-05 20:40:29 +0300

    wip

--- BioHorizon/mainapp/views.py
+++ BioHorizon/mainapp/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from services import data_handler
 
 # Create your views here.
 
 def home(request):
-    result_dict = {'expression data from drosophila melanogaster': 'Spaceflight alters Drosophila innate immune gene expression after infection.', 
-                'response of human lymphoblastoid cells to hze (iron ions) or gamma-rays': 'Transcriptomic response of TK6 cells to HZE vs gamma irradiation.'}
-    
+    data = data_handler.Data()
+    result_dict = data.get_latest_research(2)
+
     list_of_dicts = [{"title": k, "description": v} for k, v in result_dict.items()]
     return render(request, 'mainapp/index.html', {"result":list_of_dicts})
