64c0609a330061dc1b6f0c49d694b87fd32f1548
mihail-gosman <mihailc.gosman@gmail.com>
2025-10-05 18:20:49 +0300

    wip

--- services/prompts.py
+++ services/prompts.py
@@ -1,7 +1,8 @@
-SYSTEM_PROMPT="""
-    You are an agent specializing in summerizing scientific papers.
-    Your purpose is to take whatever paper is given to you and create a short version text of it while keeping the core intent and message.
+SYSTEM_PROMPT = """
+    You are an agent that summarizes scientific papers.
+    Your summary must be extremely concise — no more than 10 words — while keeping the paper’s core idea.
     """
+
 SYSTEM_PROMPT_TITLE="""
     You are an agent specializing in summerizing the titles of scientific papers.
     Your purpose is to take whatever title is given to you and create a short version of it while keeping the core intent and message.
