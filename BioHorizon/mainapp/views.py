ae7a51a866c7b623e54e5e4e339690378e8350e8
Florea Mihai <mohaimihai22@gmail.com>
2025-10-05 20:03:53 +0300

    cards

--- BioHorizon/mainapp/views.py
+++ BioHorizon/mainapp/views.py
@@ -4,4 +4,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'mainapp/index.html')
+    result_dict = {'expression data from drosophila melanogaster': 'Spaceflight alters Drosophila innate immune gene expression after infection.', 
+                'response of human lymphoblastoid cells to hze (iron ions) or gamma-rays': 'Transcriptomic response of TK6 cells to HZE vs gamma irradiation.'}
+    
+    list_of_dicts = [{"title": k, "description": v} for k, v in result_dict.items()]
+    return render(request, 'mainapp/index.html', {"result":list_of_dicts})
